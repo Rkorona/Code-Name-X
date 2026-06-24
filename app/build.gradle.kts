e614c79bee7c54af6c7422150f928db34814b67b
rkorona <korona.starx@gmail.com>
2026-06-24 17:47:55 +0800

    fix other income

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,34 +1,25 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
-    namespace = "com.scripthub.app"
+    namespace = "com.example.myapplication"
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.scripthub.app"
+        applicationId = "com.example.myapplication"
         minSdk = 30
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        ndk {
-            abiFilters += "arm64-v8a"
-        }
-        externalNativeBuild {
-            cmake {
-                cppFlags("")
-            }
-        }
     }
     
     signingConfigs {
         create("release") {
+            // 从环境变量中读取路径和密码
             val keystorePath = System.getenv("KEYSTORE_PATH")
             if (!keystorePath.isNullOrEmpty()) {
                 storeFile = file(keystorePath)
@@ -53,47 +44,19 @@ android {
     }
     buildFeatures {
         compose = true
-        aidl = true
     }
     buildToolsVersion = "37.0.0"
-
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
-
-    packaging {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-    }
 }
 
 dependencies {
-    val room_version = "2.8.4"
-    
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
-
-    implementation("androidx.work:work-runtime-ktx:2.10.1")
-    implementation("org.tukaani:xz:1.9")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -101,10 +64,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.material.icons.extended)
-    
-    implementation("org.apache.commons:commons-compress:1.28.0")
-
-    implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation("dev.rikka.shizuku:provider:13.1.5")
 }
